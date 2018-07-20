682e3f2a4062eb7b232afcdb8a60dcb61ec8bd7b
teshnizi <tigerous215@gmail.com>
2018-07-20 10:28:08 +0430

    Random test_data generator

---
+++ CSV_generator.cpp
@@ -0,0 +1,34 @@
+
+/*
+ * Generates a test CSV file
+ * Each line contains a tuple in the following format:
+ * PRIMARY_KEY(order key, part key, sup key), EXTENDED_PRICE, DISCOUNT, SHIP_DATE, QUANTITY
+ */
+
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <set>
+#include <fstream>
+
+using namespace std;
+
+const int test_size = 100007;
+
+
+int main(){
+
+    ofstream fout;
+    fout.open("test_data.csv");
+
+    for (int i = 0; i < test_size; ++i) {
+        float extended_price = 0.99 + (rand()%100) * 5;
+        float discount = (rand()%20)/40.0;
+        int year = 1990 + (rand()%28);
+        int month = (rand()%12) + 1;
+        int day = rand()%30;
+        int quantity = (rand()%100) * 50 + 50;
+        fout<<i<<","<<extended_price<<","<<discount<<","<<year<<"-"<<month<<"-"<<day<<","<<quantity<<endl;
+    }
+    fout.close();
+}
