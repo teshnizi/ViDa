e3fddf99b400c4d32175dfb5db7e20ceafebbea6
teshnizi <tigerous215@gmail.com>
2018-07-20 12:56:18 +0430

    Added line_item sruct. Defined == operator.

--- tuples.h
+++ tuples.h
@@ -29,4 +29,38 @@ struct date{
         return x.day<y.day;
     }
 
+    friend bool operator==(date x, date y){
+        return ((x.year == y.year) && (x.month == y.month) && (x.day == y.day));
+    }
+
+};
+
+
+struct line_item{
+
+    int key;
+    date ship_date;
+    int extended_price;
+    float discount;
+    int quantity;
+
+    line_item(int item_key, date item_ship_date, int item_extended_price, float item_discount, int item_quantity){
+        key = item_key;
+        ship_date = item_ship_date;
+        extended_price = item_extended_price;
+        discount = item_discount;
+        quantity = item_quantity;
+    }
+
+    friend bool operator<(line_item x, line_item y){
+        return x.key < y.key;
+    }
+    friend bool operator==(line_item x, line_item y){
+        return (x.key == y.key
+                && x.ship_date == y.ship_date
+                && x.extended_price == y.extended_price
+                && x.discount == y.discount
+                && x.quantity == y.quantity);
+    }
+
 };
