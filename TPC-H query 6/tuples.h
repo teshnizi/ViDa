388b0c7a345b5ad982340e1acc159862b3065597
teshnizi <tigerous215@gmail.com>
2018-07-25 21:33:15 +0430

    Deleted date struct, added two functions to tuple.h for handling integer dates ( leap years are not calculated). deleted indexing.

--- TPC-H query 6/tuples.h
+++ TPC-H query 6/tuples.h
@@ -5,55 +5,47 @@
 #include <sstream>
 #include <string>
 
+
 using namespace std;
 
-struct date{
 
-    short year;
-    short month;
-    short day;
-    date(){
-        year = 1998;
-        month = 8;
-        day = 10;
-    }
+const int month_days_acc[] = {31, 31+28, 31+28+31, 31+28+31+30, 31+28+31+30+31,
+                              31+28+31+30+31+30, 31+28+31+30+31+30+31, 31+28+31+30+31+30+31+31,
+                              31+28+31+30+31+30+31+31+30, 31+28+31+30+31+30+31+31+30+31,
+                              31+28+31+30+31+30+31+31+30+31+30, 31+28+31+30+31+30+31+31+30+31+30+31};
 
-    date(short d_year, short d_month, short d_day){
-        year= d_year;
-        month = d_month;
-        day = d_day;
-    }
 
-    date(string s){
-        istringstream iss(s);
-        char tmp;
-        iss >> year >> tmp >> month >> tmp >> day;
-    }
+string days_to_string(int days){
+    int year = (days / 365);
+    days -= year * 365;
+    int month = (lower_bound(month_days_acc,month_days_acc+12, days+1) - month_days_acc);
+//    cout << month << endl;
+    if ( month > 0 )
+        days -= month_days_acc[month-1];
+    return to_string(year + 1900) + "-" + to_string(month + 1) + "-" + to_string(days + 1);
+}
 
-    friend bool operator<(date x, date y){
-        if ( x.year != y.year)
-            return x.year<y.year;
-        if ( x.month != y.month)
-            return x.month<y.month;
-        return x.day<y.day;
-    }
-
-    friend bool operator==(date x, date y){
-        return ((x.year == y.year) && (x.month == y.month) && (x.day == y.day));
-    }
-
-};
+int string_date_to_days(string date){
+    replace(date.begin(), date.end(), '-', ' ');
+    istringstream iss(date);
+    int year, month, day, days;
+    iss >> year >> month >> day;
+    days = (year-1900) * 365;
+    (month > 1)?days+=month_days_acc[month-2]:1+1;
+    days += day - 1;
+    return days;
+}
 
 
 struct line_item{
 
     int key;
-    string ship_date;
+    int ship_date;
     int extended_price;
     float discount;
     int quantity;
 
-    line_item(int item_key, string item_ship_date, int item_extended_price, float item_discount, int item_quantity){
+    line_item(int item_key, int item_ship_date, int item_extended_price, float item_discount, int item_quantity){
         key = item_key;
         ship_date = item_ship_date;
         extended_price = item_extended_price;
@@ -64,13 +56,13 @@ struct line_item{
     line_item(string csv){
         replace(csv.begin(), csv.end(), ',', ' ');
         istringstream iss(csv);
-        string date_string;
         iss >> key >> extended_price >> discount >> ship_date >> quantity;
     }
 
     friend bool operator<(line_item x, line_item y){
         return x.key < y.key;
     }
+
     friend bool operator==(line_item x, line_item y){
         return (x.key == y.key
                 && x.ship_date == y.ship_date
@@ -80,7 +72,7 @@ struct line_item{
     }
 
     string to_str(){
-        return "Key: " + to_string(key) + "   Ship date: " + ship_date +
+        return "Key: " + to_string(key) + "   Ship date: " + days_to_string(ship_date) +
                "  price: " + to_string(extended_price) + "  discount: " + to_string(discount) + " quantity: " + to_string(quantity);
     }
 
