8aa8724b21c8632e48db9b0d5eeee93ff2e27e10
teshnizi <tigerous215@gmail.com>
2018-10-30 16:47:26 +0330

    wolfram convolution

--- Main/tmp.cpp
+++ Main/tmp.cpp
@@ -2,11 +2,31 @@
 #include <chrono>
 #include "transmitter.h"
 
+
 int main(){
     prepare_curl();
+
     auto start = chrono::system_clock::now();
-    cout << integrate("7x/t + 3ln(xt) over t") << endl;
+
+    //here we calculate an integral:
+    cout << endl << integrate("7x/t + 3(t^2)ln(t) over t") << endl << endl;
     auto finish = chrono::system_clock::now();
+
     chrono::duration<double> diff = finish - start;
+    cout << diff.count() << endl << endl << endl;
+
+
+    string exp1 = "e^(-(x^2))";
+    string exp2 = "e^(-(x^2))";
+
+    start = chrono::system_clock::now();
+
+    //here we calculate a convolution:
+    cout << convolve(exp1, exp2) << endl << endl;
+    finish = chrono::system_clock::now();
+
+    diff = finish - start;
     cout << diff.count() << endl;
+
+    cleanup();
 }
--- Main/transmitter.h
+++ Main/transmitter.h
@@ -16,7 +16,11 @@ string WolframAppID = "H4K447-WA9Y2J6P79";
 #define codesub "-"
 #define codediv "%2F"
 #define codespace "%20"
+#define codeor "%7C"
+#define codeopenpar "%28"
+#define codeclosepar "%29"
 
+string wolfram_compute(string message);
 
 struct buffer {
     char *ptr;
@@ -33,8 +37,7 @@ void init_buffer(struct buffer *s) {
     s->ptr[0] = '\0';
 }
 
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct buffer *s)
-{
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct buffer *s) {
     size_t new_len = s->len + size*nmemb;
     s->ptr = (char*)realloc(s->ptr, new_len+1);
     if (s->ptr == NULL) {
@@ -48,8 +51,10 @@ size_t writefunc(void *ptr, size_t size, size_t nmemb, struct buffer *s)
     return size*nmemb;
 }
 
-string cast_to_integral_URL(string expression){
-    string ret = "http://api.wolframalpha.com/v2/query?appid=" + WolframAppID + "&input=integrate%20";
+string normalize_expression(string expression){
+
+    string ret = "";
+
     for (int i = 0; i < expression.length(); i++){
         switch (expression[i]){
             case ' ':
@@ -70,37 +75,66 @@ string cast_to_integral_URL(string expression){
             case '/':
                 ret += codediv;
                 break;
+            case '|':
+                ret += codeor;
+                break;
+            case '(':
+                ret += codeopenpar;
+                break;
+            case ')':
+                ret += codeclosepar;
+                break;
             default:
                 ret += expression[i];
         }
     }
-    return ret+"&includepodid=IndefiniteIntegral";
+    return ret;
+}
+
+void replaceAll(string& str, const string& from, const string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+}
+
+string integrate(string expression){
+    string ret = "http://api.wolframalpha.com/v2/result?appid=" + WolframAppID + "&i=integrate%20";
+    ret += normalize_expression(expression);
+    return wolfram_compute(ret);
+}
+
+string convolve(string exp1, string exp2){
+    string ret = "http://api.wolframalpha.com/v2/result?appid=" + WolframAppID + "&i=";
+    ret += normalize_expression("convolve(" + exp1 + "," + exp2 + ")");
+    return wolfram_compute(ret);
 }
 
 void prepare_curl(){
     curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 }
 
 void cleanup(){
-    curl_easy_cleanup(curl);
     curl_global_cleanup();
 }
 
-string integrate(string message){
+buffer b;
 
-    CURL *curl;
+string wolfram_compute(string message){
+
     CURLcode res;
-    string ans;
+    string ans = "";
 
     curl = curl_easy_init();
     if(curl) {
-        string s = cast_to_integral_URL(message);
-        cout << s << endl;
-        buffer b;
 
-        curl_easy_setopt(curl, CURLOPT_URL, cast_to_integral_URL(message).c_str());
+        init_buffer(&b);
+//        cout << cast_to_integral_URL(message) << endl;
+        curl_easy_setopt(curl, CURLOPT_URL, message.c_str());
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &b);
@@ -111,26 +145,8 @@ string integrate(string message){
                     curl_easy_strerror(res));
 
 //        printf("%s", b.ptr);
-        stringstream ss(b.ptr);
-        string line;
-        while (getline(ss, line)){
-            if (line.length() < 25)continue;
-            if (line[14] == 'i' &&
-                        line[15] == 'n' &&
-                        line[16] == 't' &&
-                        line[17] == 'e' &&
-                        line[18] == 'g'){
-                int it = 14;
-                while (line[it++] != '=');
-                it++;
-                int it2 = it;
-                cout << line << endl;
-                while (!(line[it2++] == 'c' && line[it2+1] != 'o'));
-                ans = line.substr(it, it2 - it - 4);
-                break;
-            }
-        }
         curl_easy_cleanup(curl);
+        return string(b.ptr);
     }
     return ans;
 }
