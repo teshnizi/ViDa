578a96420ca423ad258b204a2c0e00e46edbb49d
Ali AhmadiTeshnizi <tigerous215@gmail.com>
2019-10-19 12:39:19 +0000

    Update Q6.cpp

--- Main/Sample Generated Codes/Q6.cpp
+++ Main/Sample Generated Codes/Q6.cpp
@@ -8,34 +8,34 @@ using namespace std;
 int main(){
 
 
-	int lineitem_ia[] = {l_discount_id, l_price_id, l_shipdate_id, l_quantity_id};
-	int lineitem_sa[] = {l_shipinstruct_id, l_shipmode_id};
+	int lineitem_ia[] = {ldiscount_id, lprice_id, lshipdate_id, lquantity_id};
+	int lineitem_sa[] = {lshipinstruct_id, lshipmode_id};
     read_lineitems_from_file("edited_lineitem.tbl", lineitem_ia, 4, lineitem_sa, 2);
     read_histogram_defaults_from_file("histograms.txt");
 	//SCANNING ATTRIBUTES(TAKING A COPY FROM DEFAULTS)
 
 	{
-		Histogram l_discount_hist;
-		l_discount_hist.minimum = default_histogram[lineitem][l_discount_id].minimum;
-		l_discount_hist.maximum = default_histogram[lineitem][l_discount_id].maximum;
-		l_discount_hist.bucket_count = default_histogram[lineitem][l_discount_id].bucket_count;
-		l_discount_hist.bucket_size = default_histogram[lineitem][l_discount_id].bucket_size;
-		for (int i = 0; i < l_discount_hist.bucket_count; ++i){
-		    l_discount_hist.bucket[i] = default_histogram[lineitem][l_discount_id].bucket[i];
+		Histogram ldiscount_hist;
+		ldiscount_hist.minimum = default_histogram[lineitem][ldiscount_id].minimum;
+		ldiscount_hist.maximum = default_histogram[lineitem][ldiscount_id].maximum;
+		ldiscount_hist.bucket_count = default_histogram[lineitem][ldiscount_id].bucket_count;
+		ldiscount_hist.bucket_size = default_histogram[lineitem][ldiscount_id].bucket_size;
+		for (int i = 0; i < ldiscount_hist.bucket_count; ++i){
+		    ldiscount_hist.bucket[i] = default_histogram[lineitem][ldiscount_id].bucket[i];
 		}
 		for (int lineitem_it = 0; lineitem_it < table_size[lineitem]; lineitem_it++)
 		{
-			int l_partkey = int_table[lineitem][l_partkey_id][lineitem_it];
+			int lpartkey = int_table[lineitem][lpartkey_id][lineitem_it];
 
-			int l_price = int_table[lineitem][l_price_id][lineitem_it];
+			int lprice = int_table[lineitem][lprice_id][lineitem_it];
 
-			int l_quantity = int_table[lineitem][l_quantity_id][lineitem_it];
+			int lquantity = int_table[lineitem][lquantity_id][lineitem_it];
 
-			int l_shipdate = int_table[lineitem][l_shipdate_id][lineitem_it];
+			int lshipdate = int_table[lineitem][lshipdate_id][lineitem_it];
 
-			string l_shipinstruct = string_table[lineitem][l_shipinstruct_id][lineitem_it];
+			string lshipinstruct = string_table[lineitem][lshipinstruct_id][lineitem_it];
 
-			string l_shipmode = string_table[lineitem][l_shipmode_id][lineitem_it];
+			string lshipmode = string_table[lineitem][lshipmode_id][lineitem_it];
 
 			double lineitem_table_size = 1;
 			//APPLYING THE CONDITIONS OF EVERY SUBCONDITION INSIDE SELECT NODE
@@ -45,53 +45,53 @@ int main(){
 
 				int Select_lineitem_table_size = lineitem_table_size;
 
-				Histogram Select_l_discount_hist;
-				copy_hist(&Select_l_discount_hist, &l_discount_hist);
+				Histogram Select_ldiscount_hist;
+				copy_hist(&Select_ldiscount_hist, &ldiscount_hist);
 
-				int Select_l_partkey = l_partkey;
+				int Select_lpartkey = lpartkey;
 
-				int Select_l_price = l_price;
+				int Select_lprice = lprice;
 
-				int Select_l_quantity = l_quantity;
+				int Select_lquantity = lquantity;
 
-				int Select_l_shipdate = l_shipdate;
+				int Select_lshipdate = lshipdate;
 
-				string Select_l_shipinstruct = l_shipinstruct;
+				string Select_lshipinstruct = lshipinstruct;
 
-				string Select_l_shipmode = l_shipmode;
+				string Select_lshipmode = lshipmode;
 
 
 				//APPLYING SUBCONDITIONS
 
-				if(1 <= l_quantity && l_quantity < 24)
+				if(1 <= lquantity && lquantity < 24)
 				{
 
-					lineitem_table_size *= limit(&l_discount_hist, 6, 8);
+					lineitem_table_size *= limit(&ldiscount_hist, 6, 8);
 
-					if(34310 <= l_shipdate && l_shipdate < 34675)
+					if(34310 <= lshipdate && lshipdate < 34675)
 					{
 
 
 
-						sum_price += data_data_mult_data(1 * lineitem_table_size , data_hist_mult_data(l_price, l_discount_hist));
+						sum_price += data_data_mult_data(1 * lineitem_table_size , data_hist_mult_data(lprice, ldiscount_hist));
 					}
 				}
 				//RETRIEVING ATTRIBUTES
 
 				lineitem_table_size= Select_lineitem_table_size;
 
-				copy_hist(&l_discount_hist, &Select_l_discount_hist);
-				l_partkey = Select_l_partkey;
+				copy_hist(&ldiscount_hist, &Select_ldiscount_hist);
+				lpartkey = Select_lpartkey;
 
-				l_price = Select_l_price;
+				lprice = Select_lprice;
 
-				l_quantity = Select_l_quantity;
+				lquantity = Select_lquantity;
 
-				l_shipdate = Select_l_shipdate;
+				lshipdate = Select_lshipdate;
 
-				l_shipinstruct = Select_l_shipinstruct;
+				lshipinstruct = Select_lshipinstruct;
 
-				l_shipmode = Select_l_shipmode;
+				lshipmode = Select_lshipmode;
 
 
 			}
